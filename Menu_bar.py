7037f506faaf8c0b5b226ca17caa8727425c75ac
Asmee-tech <asmee.khimani@gmail.com>
2026-06-06 10:49:40 +0200

    Add files via upload

---
+++ Menu_bar.py
@@ -0,0 +1,39 @@
+from tkinter import*
+from tkinter.ttk import*
+
+screen=Tk()
+screen.title("Menu bar")
+
+# creating menu bar
+menubar=Menu(screen)
+
+# adding file menu and commands
+file=Menu(menubar,tearoff=0)
+menubar.add_cascade(label='File',menu=file)
+file.add_command(label='New File',command=None)
+file.add_command(label='Open',command=None)
+file.add_command(label='Save',command=None)
+file.add_separator()
+file.add_command(label='Exit',command=screen.destroy)
+screen.config(menu=menubar)
+
+# adding the edit commands
+edit=Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Edit',menu=edit)
+edit.add_command(label='Cut',command=None)
+edit.add_command(label='Copy',command=None)
+edit.add_command(label='Paste',command=None)
+edit.add_command(label='Select All',command=None)
+edit.add_separator()
+edit.add_command(label='Find',command=None)
+edit.add_command(label='Find again',command=None)
+
+# adding the help commands
+help=Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Help',menu=help)
+help.add_command(label='TK Help',command=None)
+help.add_command(label='Demo',command=None)
+help.add_separator()
+help.add_command(label='About Tk',command=None)
+
+screen.mainloop()
